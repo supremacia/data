16be84400239d08b5371488eff7fdc8b75fe0df8
Bill Rocha <Bill Rocha>
2016-03-22 12:07:28 -0300

    Outono Orange

--- Db.php
+++ Db.php
@@ -36,8 +36,8 @@
 namespace Limp\Data;
 use PDO;
 
-class Db {
-
+class Db 
+{
     private $dsn = 'sqlite:qzumba.db';
     private $user = null;
     private $passw = null;
@@ -49,8 +49,8 @@ class Db {
     private $error = [];
 
 
-
-    function __construct($cfg = false){
+    function __construct($cfg = false)
+    {
         if(is_array($cfg)){
             $this->dsn = $cfg['dsn'];
             $this->user = $cfg['user'];
@@ -58,7 +58,8 @@ class Db {
         }
     }
 
-    function connect(){
+    function connect()
+    {
         if($this->conn == null)
             try{
             $this->conn = new PDO(  $this->dsn,
@@ -72,8 +73,8 @@ class Db {
         return $this->conn;
     }
 
-
-    function query($sql,$parms = array()){
+    function query($sql, $parms = array())
+    {
         $this->sql = $sql;
         $sth = $this->connect()->prepare($sql);
         $sth->execute($parms);
@@ -81,7 +82,7 @@ class Db {
         $this->error[$sql] = $sth->errorInfo();
 
         if($sth->columnCount() > 0) {
-            return $this->result = $sth->fetchAll(PDO::FETCH_CLASS,"Lib\Row");
+            return $this->result = $sth->fetchAll(PDO::FETCH_CLASS,"Limp\Data\Row", [$this->sql, $parms]);
         } else {
             $this->result = false;
             return $this->rows;
@@ -89,27 +90,32 @@ class Db {
     }
 
     //Result Object
-    function result(){
+    function result()
+    {
         if($this->result == null || count($this->result) == 0) return false;
         return $this->result;
     }
     //Limpa os resultados
-    function clear(){
+    function clear()
+    {
         $this->result = new Row;
     }
 
     //Pegando erros
-    function getError(){
+    function getError()
+    {
         return $this->error;
     }
 
     //return the number of rows affected by the last DELETE, INSERT or UPDATE
-    function getRows(){
+    function getRows()
+    {
         return $this->rows;
     }
 
     //return last sql string
-    function getSql(){
+    function getSql()
+    {
         return $this->sql;
     }
 
@@ -120,20 +126,33 @@ class Db {
  *
  * @author http://plus.google.com/+BillRocha
  */
-class Row {
+class Row 
+{
+    private $__columns = [];
+    private $__rowParms = ['table'=>null,
+                           'where'=>null,
+                           'sql'=>null,
+                           'parms'=>null,
+                           'id'=>null
+                          ];
 
-    private $__table = null;
-    private $__where = null;
-    private $id = null;
 
+    function __construct($sql, $parms)
+    { 
+        $this->__rowParms['sql'] = $sql;
+        $this->__rowParms['parms'] = $parms;
 
-    function __construct(){
-        //TODO: null
+        foreach($this as $n=>$v){
+            if($n == '__rowParms' || $n == '__columns') continue;
+            $this->__columns[$n] = $v;
+            unset($this->{$n});
+        }
     }
 
 
     //Salva os dados no banco de dados [insert/update]
-    function save(){
+    function save()
+    {
         //if($this->id == null) //INSERT INTO
         //else //UPDATE
 
@@ -150,14 +169,27 @@ class Row {
      * @param string $parm
      * @return boolean
      */
-    function get($parm = false){
-        if(isset($this->$parm)) return $this->$parm;
+    function get($parm = false)
+    {
+        if(isset($this->__columns[$parm])) return $this->__columns[$parm];
         return false;
     }
 
+
+    function first()
+    {
+        return reset($this->__columns);
+    }
+
+    function next()
+    {
+        return next($this->__columns);
+    }
+
     //Return $this as array
-    function getArray(){
-        foreach ($this as $k=>$v) {
+    function getAll()
+    {
+        foreach ($this->__columns as $k=>$v) {
             $a[$k] = $v;
         }
         return $a;
@@ -169,13 +201,14 @@ class Row {
      * @param mixed $value Value of parameter
      * @return boolean
      */
-    function set($parm, $value = null){
+    function set($parm, $value = null)
+    {
         if(is_array($parm)){
-            foreach($parm as $k=>$v){ $this->$k = $v; }
+            foreach($parm as $k=>$v){ $this->__columns[$k] = $v; }
             return $this;
         }
-        elseif(isset($this->$parm)) {
-            $this->$parm = $value;
+        elseif(isset($this->__columns[$parm])) {
+            $this->__columns[$parm] = $value;
             return $this;
         }
         else return false;
